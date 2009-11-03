6812bfba05753f0520d045680ad4fff36dcf7d2c
Danny Hawkins <djh@hawkinsking.com>
2009-11-03 10:32:47 +0000

    added

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,9 +1,11 @@
 # Settings specified here will take precedence over those in config/environment.rb
-config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
-config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0" 
-config.gem "cucumber", :lib => false, :version => "= 0.3.98"
-config.gem "thoughtbot-factory_girl", :lib    => "factory_girl", :source => "http://gems.github.com"
-config.gem "webrat", :lib => false, :version => ">= 0.5.1"
+config.gem "rspec", :lib => false, :version => ">= 1.2.9" 
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.9" 
+config.gem "cucumber", :lib => false, :version => ">=0.4.3"
+config.gem "pickle", :lib => false, :version => ">=0.1.21"
+
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+config.gem "webrat", :lib => false, :version => ">= 0.5.3"
 config.gem "nokogiri", :lib => false, :version => ">= 1.2.3"
 config.gem "selenium-client", :lib => false, :version => ">= 1.2.16"
 
